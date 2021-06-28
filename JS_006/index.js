7cac7dd06603fb71e2c1d48e55749c2bc7ddc7a2
NdBR <84131697+NdBR@users.noreply.github.com>
2021-06-28 12:45:50 +0200

    Add files via upload

---
+++ JS_006/index.js
@@ -0,0 +1,21 @@
+const makePizza = function () {
+    console.log("get all ingredients");
+    console.log("make dough");
+    console.log("Make sauce");
+    console.log("top the pizza");
+    console.log("put it in the oven");
+    console.log("serve");
+};
+
+const makeSushi = function () {
+    console.log("boil the rice");
+    console.log("cut the ingredients");
+    console.log("roll the sushi");
+    console.log("ad sause and topping");
+    console.log("serve");
+}
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
